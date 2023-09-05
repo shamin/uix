cc9d218d11feab2265c181abf743c9ea5b6a3e63
roman01la <roman01la@romanliutikov.com>
2023-09-05 19:33:29 +0300

    wip

--- example-out/process/env.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.10.739 {:static-fns true, :fn-invoke-direct true, :optimize-constants true}
-goog.provide('process.env');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
-
-/**
- * @define {string}
- */
-process.env.NODE_ENV = goog.define("process.env.NODE_ENV","development");
